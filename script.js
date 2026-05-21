69a126b0ea45e9c2add605b6d240dad5c4575b9f
eunyu <a59191877@gmail.com>
2026-05-21 15:41:34 +0900

    2nd portfolio update

---
+++ script.js
@@ -0,0 +1,38 @@
+const toggleButton = document.getElementById("darkModeToggle");
+
+toggleButton.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+});
+
+const sections = document.querySelectorAll("section");
+
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop;
+
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop - 150) {
+
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === `#${current}`) {
+
+            link.classList.add("active");
+        }
+    });
+});
